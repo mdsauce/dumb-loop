54f81f2627742abe6a5e1b5644dd02cbfdca504a
mdsauce <max.dobeck@saucelabs.com>
2018-08-31 21:22:15 -0700

    Implemented naive thread test.

--- 1000-simple-selenium.py
+++ 1000-simple-selenium.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+import threading
+import queue
 from selenium.common.exceptions import WebDriverException
 from sauceclient import SauceClient
 import random
@@ -22,21 +24,43 @@ def capsBuilder():
         'name': testName
     }
     return caps
-desired_caps = capsBuilder()
 
-try: 
-    driver = webdriver.Remote(command_executor="https://%s:%s@ondemand.saucelabs.com/wd/hub" % (username, access_key), desired_capabilities=desired_caps)
-    driver.get("https://saucelabs.com")
-    if driver.title == "Cross Browser Testing, Selenium Testing, and Mobile Testing | Sauce Labs":
-        sauce_client.jobs.update_job(driver.session_id, passed=True)
-        print(f"Session {driver.session_id} passed")
-    else:
-        sauce_client.jobs.update_job(driver.session_id, passed=False)
-        print(f"Session {driver.session_id} failed")
-except WebDriverException as m:
-        print(f"Session {driver.session_id} failed for some reason!")
-        sauce_client.jobs.update_job(driver.session_id, passed=False)
-        print(m)
+def randomTest(caps):
+    try:
+        driver = webdriver.Remote(command_executor="https://%s:%s@ondemand.saucelabs.com/wd/hub" % (username, access_key), desired_capabilities=caps)
+        driver.get("https://saucelabs.com")
+        if driver.title == "Cross Browser Testing, Selenium Testing, and Mobile Testing | Sauce Labs":
+            sauce_client.jobs.update_job(driver.session_id, passed=True)
+            print(f"Session {driver.session_id} passed")
+        else:
+            sauce_client.jobs.update_job(driver.session_id, passed=False)
+            print(f"Session {driver.session_id} failed")
         driver.quit()
+    except WebDriverException as m:
+            print(f"Session {driver.session_id} failed for some reason!")
+            sauce_client.jobs.update_job(driver.session_id, passed=False)
+            print(m)
+            driver.quit()
 
-driver.quit()
+lock = threading.Lock()
+
+def worker(q):
+    global jobNumber
+    while True:
+        randomTest(q.get())
+        with lock:
+            jobNumber+=1
+            print(f"Finished job number: {jobNumber}")
+        q.task_done()
+
+capsToTest = queue.Queue()
+for i in range(1000):
+    capsToTest.put(capsBuilder())
+
+jobNumber = 0
+
+for i in range(100):
+    w = threading.Thread(target=worker, args=(capsToTest,))
+    w.setDaemon(True)
+    w.start()
+capsToTest.join()
